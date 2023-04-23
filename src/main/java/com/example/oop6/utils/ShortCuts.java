54ebb0fd31602fe216cf588ec635d4634b3f2247
aleksejmoksancev <mokshantsev.aleksei>
2023-04-23 16:44:31 +0500

    add most better save way and add enum ShortCuts for better clean code

---
+++ src/main/java/com/example/oop6/utils/ShortCuts.java
@@ -0,0 +1,34 @@
+package com.example.oop6.utils;
+
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.KeyCode;
+
+public enum ShortCuts {
+    CREATE(KeyCode.V, "Создание"),
+    SELECT(KeyCode.B, "Выделение"),
+    MOVE(KeyCode.N, "Перемещение"),
+    SIZE(KeyCode.M, "Размер"),
+    COLOR(KeyCode.C, "Цвет"),
+    GROUP(KeyCode.G, "Группировка"),
+    UNGROUP(KeyCode.F, "Расформировать группу"),
+    DELETE(KeyCode.BACK_SPACE, "Удалить"),
+    STEP_BACK(KeyCode.Z, "Шаг назад"),
+    STEP_FORWARD(KeyCode.X, "Шаг вперед"),
+    CIRCLE(KeyCode.DIGIT1, "Эллипс"),
+    RECTANGLE(KeyCode.DIGIT2, "Прямоугольник"),
+    TRIANGLE(KeyCode.DIGIT3, "Треугольник");
+
+
+
+    private final KeyCode keyCode;
+    private final String toolName;
+    ShortCuts(KeyCode keyCode, String toolName){
+        this.keyCode = keyCode;
+        this.toolName = toolName;
+    }
+
+    public Tooltip getToolTip(){
+        return new Tooltip(toolName + " '" + keyCode.getName() + "'");
+    }
+
+}
